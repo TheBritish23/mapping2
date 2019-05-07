a314403acfc1013e38f5aca0095e6427d80e9087
Soro Cat <sorousha326@gmail.com>
2019-05-07 13:37:22 -0700

    add changes in js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   $('form#list').submit(function(event){
     event.preventDefault();
 
-    $("How to get away with murder or manslaughter.").append()
 
     // var str = 'How to get away with murder or manslaughter.'
 
@@ -22,18 +21,22 @@ $(document).ready(function() {
     var inputArray = input.split(" ")
     inputArray.sort()
     var newArray = inputArray.map(function(item) {
-      return item.toUpperCase()
+      if (item.length >=3) {
+        return item.toUpperCase()
+      }
     });
 
-    var array1 = ['How', 'Away', 'Murder'];
+    var array1 = ['1', '2', '3'];
     console.log('array1: ', array1);
-    // expected output: Array ['How', 'Away', 'Murder']
+    // expected output: Array ['1', '2', '3']
 
-    var reversed = array1.reverse();
+    var reversed = newArray.reverse().join(" ");
     console.log('reversed: ', reversed);
-    // expected output: Array ['Murder', 'Away', 'How']
+    // expected output: Array ['3', '2', '1']
 
-    console.log('array1: ', reversed.join(" "));
-    // expected output: Array ['Murder', 'Away', 'How']
+    console.log('array1: ', reversed);
+    // expected output: Array ['3', '2', '1']
+
+    $("#output").append(reversed);
   });
 });
